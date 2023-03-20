b3e20c8ce1612d09f4ee4ec3c778ce1e29aa29dc
romeuic <casarottori@gmail.com>
2023-03-20 00:49:28 -0300

    adiciona estilo detalhes

--- client/src/pages/CourseDetailsPage/CourseDetailsPage.jsx
+++ client/src/pages/CourseDetailsPage/CourseDetailsPage.jsx
@@ -8,20 +8,22 @@ function CourseDetailsPage() {
 
   return (
     <div className="courseDetailsPageContainer">
-      <h2>Detalhes do Curso</h2>
-      <img src={course?.imageUrl} alt={`Imagem do curso ${course?.name}`} />
-      <p>
-        <strong>Nome:</strong> <span>{course?.name}</span>
-      </p>
-      <p>
-        <strong>Categoria:</strong> <span>{course?.category}</span>
-      </p>
-      <p>
-        <strong>Descrição:</strong> <span>{course?.description}</span>
-      </p>
-      <p>
-        <strong>Duração:</strong> <span>{course?.duration}h</span>
-      </p>
+      <div className="courseDetailsCenterBox">
+        <h2>Detalhes do Curso</h2>
+        <img src={course?.imageUrl} alt={`Imagem do curso ${course?.name}`} />
+        <p>
+          <strong>Nome:</strong> <span>{course?.name}</span>
+        </p>
+        <p>
+          <strong>Categoria:</strong> <span>{course?.category}</span>
+        </p>
+        <p>
+          <strong>Descrição:</strong> <span>{course?.description}</span>
+        </p>
+        <p>
+          <strong>Duração:</strong> <span>{course?.duration}h</span>
+        </p>
+      </div>
     </div>
   );
 }

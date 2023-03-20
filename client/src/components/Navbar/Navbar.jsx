babdd4bd657b65a7e3d461292965b7880c8895cd
romeuic <casarottori@gmail.com>
2023-03-20 00:30:14 -0300

    ajusta botao navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Button, { BUTTON_VARIANT } from "../Button";
 import { useUserInfo, useSetUserInfo } from "../../hooks/useUserInfo";
 import "./Navbar.css";
 
 function Navbar() {
+  const location = useLocation();
   const navigate = useNavigate();
   const setUserInfo = useSetUserInfo();
   const userInfo = useUserInfo();
+  const isLoggedIn = userInfo?.name;
+
+  console.log(location);
 
   const handleNavigateLogin = () => {
     navigate("/login");
@@ -22,10 +27,13 @@ function Navbar() {
         <img src="/logo.png" alt="Logo" />
         <h1>DEVinCursos</h1>
       </Link>
-      {userInfo?.name ? (
-        <button onClick={handleLogout}>Sair</button>
-      ) : (
-        <button onClick={handleNavigateLogin}>Entrar</button>
+      {location.pathname !== "/login" && (
+        <Button
+          variant={BUTTON_VARIANT.PRIMARY_OUTLINED}
+          onClick={isLoggedIn ? handleLogout : handleNavigateLogin}
+        >
+          {isLoggedIn ? "Sair" : "Entrar"}
+        </Button>
       )}
     </header>
   );

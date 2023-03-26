26b04c3a530315c49fa5dd0eb621d16ac04553c2
zanoniFab <fabianezanoni@gmail.com>
2023-03-26 19:56:39 -0300

    exercicio 8: cria tela de login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,25 @@
 import Navbar from './components/Navbar';
 import PageWrapper from './components/PageWrapper';
 import Footer from './components/Footer';
-import { GlobalStateProvider } from './hooks/useGlobalState';
 import HomePage from './pages/HomePage';
-import { useState } from "react";
 import CourseDetailsPage from "./pages/CourseDetailsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
-  const [globalState, setGlobalState] = useState({
-    user: {
-      name: "Fabiane",
-      isAdmin: true,
-    },
-  });
-
   return (
-    
-      <GlobalStateProvider value={[globalState, setGlobalState]}>
-        <BrowserRouter>
-          <Navbar />
-            <PageWrapper>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/course/:id" element={<CourseDetailsPage />} />
-                <Route path="*" element={<div>Página não encontrada</div>} />
-              </Routes>
-            </PageWrapper>
-          <Footer />
-        </BrowserRouter>
-      </GlobalStateProvider>
+      <BrowserRouter>
+        <Navbar />
+          <PageWrapper>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/course/:id" element={<CourseDetailsPage />} />
+              <Route path="*" element={<div>Página não encontrada</div>} />
+            </Routes>
+          </PageWrapper>
+        <Footer />
+      </BrowserRouter>
   );
 }
 

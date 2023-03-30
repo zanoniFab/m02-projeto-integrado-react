6803bc6068034066fe2a6503caa0617513234917
zanoniFab <fabianezanoni@gmail.com>
2023-03-30 20:09:19 -0300

    Exercicio 9: ajusta o header para exibir entrar ou sair

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import PageWrapper from './components/PageWrapper';
 import Footer from './components/Footer';
+import { GlobalStateProvider } from './hooks/useGlobalState';
 import HomePage from './pages/HomePage';
-import CourseDetailsPage from "./pages/CourseDetailsPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
+import CourseDetailsPage from "./pages/CourseDetailsPage";
+import { useState } from 'react'
 function App() {
+  const [globalState, setGlobalState] = useState()
   return (
+    <GlobalStateProvider value={[globalState, setGlobalState]}>
       <BrowserRouter>
         <Navbar />
           <PageWrapper>
@@ -20,6 +23,7 @@ function App() {
           </PageWrapper>
         <Footer />
       </BrowserRouter>
+    </GlobalStateProvider>
   );
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
 import './Navbar.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserInfo, useSetUserInfo } from "../../hooks/useUserInfo";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const setUserInfo = useSetUserInfo();
+  const userInfo = useUserInfo();
+
+  const handleNavigateLogin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    setUserInfo();
+    handleNavigateLogin();
+  };
+
   return (
     <header className='navbarContainer'>
       <Link to="/" className="navbarLogoBox">
         <img src='/logo.png' alt='Logo' />
         <h1>DEVinCursos</h1>
       </Link>
+      {userInfo?.name ? (
+        <button onClick={handleLogout}>Sair</button>
+      ) : (
+        <button onClick={handleNavigateLogin}>Entrar</button>
+      )}
     </header>
   );
 }

8c5e9e7456888eca1c1bfd707d0671011e912d61
zanoniFab <fabianezanoni@gmail.com>
2023-03-31 19:45:58 -0300

    Exercicio 1: instala e configura styled-components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { GlobalStyles } from "./styles";
+ReactDOM.createRoot(document.getElementById("root")).render(
+<>
+    <GlobalStyles />
+    <App />
+</>
+    );
